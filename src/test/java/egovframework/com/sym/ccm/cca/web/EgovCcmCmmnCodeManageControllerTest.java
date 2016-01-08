c377888e8028a315cccd8f0373c4ab14f57e1286
jhong <jhsign@gmail.com>
2016-01-09 00:19:19 +0900

    spring 4 controller 테스트 위해 MockMvc 사용

--- src/test/java/egovframework/com/sym/ccm/cca/web/EgovCcmCmmnCodeManageControllerTest.java
+++ src/test/java/egovframework/com/sym/ccm/cca/web/EgovCcmCmmnCodeManageControllerTest.java
@@ -1,9 +1,12 @@
 package egovframework.com.sym.ccm.cca.web;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,9 +16,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 /**
@@ -25,29 +30,14 @@ import org.springframework.web.context.WebApplicationContext;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-//@ContextConfiguration( locations = { "classpath*:egovframeworktest/spring/com/**/context-*.xml" } )
-//@ContextConfiguration( locations = { "file:src/main/webapp/WEB-INF/config/egovframework/springmvc/egov-com-servlet.xml" } )
-@ContextConfiguration( locations = { "classpath:egovframeworktest/springmvc/egov-com-servlet.xml" } )
-//@TransactionConfiguration(transactionManager = "txManager", defaultRollback = true)
-//@Transactional
+@ContextConfiguration( locations = { "classpath*:egovframeworktest/springmvc/egov-com-servlet.xml",
+									"classpath*:egovframeworktest/spring/com/**/context-*.xml"} )
+@TransactionConfiguration(transactionManager = "txManager", defaultRollback = true)
+@Transactional
 public class EgovCcmCmmnCodeManageControllerTest {
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
-//	private MockHttpServletRequest request; 
-//	private MockHttpServletResponse response; 
-	
-//	@Mock
-//	private BindingResult mockBindingResult;
-	
-//	@Autowired
-//	private RequestMappingHandlerAdapter handlerAdapter;
-//	
-//	@Autowired
-//	private RequestMappingHandlerMapping handlerMapping;
-	
-//	@Autowired
-//	private EgovCcmCmmnCodeManageController controller;
 
 	@Autowired
 	private WebApplicationContext context;
@@ -57,8 +47,58 @@ public class EgovCcmCmmnCodeManageControllerTest {
 	@Before
 	public void setup(){
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
-//		assertThat(controller, is(notNullValue()));
+//	    this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).dispatchOptions(true).build();
+//		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).alwaysExpect(forwardedUrl(null)).build();
 	}
+
+	/**
+	 * deleteCmmnCode() test
+	 * @throws Exception
+	 */
+	@Test
+	public void deleteCmmnCode() throws Exception{
+		 this.mockMvc.perform(post("/sym/ccm/cca/EgovCcmCmmnCodeRemove.do")
+				 .param("codeId", "TST002"))
+		 .andDo(print())
+		 .andExpect(status().isOk())
+		 .andExpect(model().attributeExists("cmmnCode"))
+		 .andExpect(view().name("forward:/sym/ccm/cca/EgovCcmCmmnCodeList.do"))
+		 .andExpect(forwardedUrl("/sym/ccm/cca/EgovCcmCmmnCodeList.do"));
+	}	
+
+	/**
+	 * insertCmmnCode() test
+	 * @throws Exception
+	 */
+	@Test
+	public void insertCmmnCode() throws Exception{
+		 this.mockMvc.perform(post("/sym/ccm/cca/EgovCcmCmmnCodeRegist.do")
+				 .param("codeId", "TST002")
+				 .param("codeIdNm", "이력구분 TEST")
+				 .param("codeIdDc", "시스템이력등록구분 TEST")
+				 .param("clCode", "EFC")
+				 .param("useAt", "Y")
+				 .param("lastUpdusrId", "tester"))
+		 .andDo(print())
+		 .andExpect(status().isOk())
+		 .andExpect(model().attributeExists("cmmnCode"))
+		 .andExpect(view().name("forward:/sym/ccm/cca/EgovCcmCmmnCodeList.do"))
+		 .andExpect(forwardedUrl("/sym/ccm/cca/EgovCcmCmmnCodeList.do"));
+	}	
+
+	/**
+	 * selectCmmnCodeDetail() test
+	 * @throws Exception
+	 */
+	@Test
+	public void selectCmmnCodeDetail() throws Exception{
+		 this.mockMvc.perform(get("/sym/ccm/cca/EgovCcmCmmnCodeDetail.do").param("codeId", "COM002"))
+		 .andDo(print())
+		 .andExpect(status().isOk())
+		 .andExpect(model().attributeExists("result"))
+		 .andExpect(view().name("egovframework/com/sym/ccm/cca/EgovCcmCmmnCodeDetail"))
+		 .andExpect(forwardedUrl("/WEB-INF/jsp/egovframework/com/sym/ccm/cca/EgovCcmCmmnCodeDetail.jsp"));
+	}	
 
 	/**
 	 * selectCmmnCodeList() test
@@ -66,33 +106,33 @@ public class EgovCcmCmmnCodeManageControllerTest {
 	 */
 	@Test
 	public void selectCmmnCodeList() throws Exception{
-		 this.mockMvc.perform(get("/"))
+		 this.mockMvc.perform(get("/sym/ccm/cca/EgovCcmCmmnCodeList.do"))
 		 .andDo(print())
-		 .andExpect(status().isOk());
-//		 .andExpect(model().attributeExists("egovframework/com/sym/ccm/cca/EgovCcmCmmnCodeList"));
+		 .andExpect(status().isOk())
+		 .andExpect(model().attributeExists("resultList"))
+		 .andExpect(view().name("egovframework/com/sym/ccm/cca/EgovCcmCmmnCodeList"))
+		 .andExpect(forwardedUrl("/WEB-INF/jsp/egovframework/com/sym/ccm/cca/EgovCcmCmmnCodeList.jsp"));
 	}	
-//	@Before
-//	public void setUp() throws Exception {
-////		request = new MockHttpServletRequest();
-////		response = new MockHttpServletResponse();
-//		
-//		assertThat(controller, is(notNullValue()));
-//	}
 
-//	/**
-//	 * selectCmmnCodeList() test
-//	 * @throws Exception
-//	 */
-//	@Test
-//	public void selectCmmnCodeList() throws Exception {
-//		
-//		// parameters
-////		request.setRequestURI("/sym/ccm/cca/EgovCcmCmmnCodeList.do");
-//		
-////		ModelAndView mav = new AnnotationMethodHandlerAdapter().handle(request, response, controller);
-////		assertThat(mav.getViewName(), is("egovframework/com/sym/ccm/cca/EgovCcmCmmnCodeList"));
-//		
-//		
-//	}
+	/**
+	 * updateCmmnCode() test
+	 * @throws Exception
+	 */
+	@Test
+	public void updateCmmnCode() throws Exception{
+		 this.mockMvc.perform(post("/sym/ccm/cca/EgovCcmCmmnCodeModify.do")
+				 .param("cmd", "Modify")
+				 .param("codeId", "COM002")
+				 .param("codeIdNm", "이력구분 A")
+				 .param("codeIdDc", "시스템이력등록구분 B")
+				 .param("clCode", "EFC")
+				 .param("useAt", "Y")
+				 .param("lastUpdusrId", "tester"))
+		 .andDo(print())
+		 .andExpect(status().isOk())
+		 .andExpect(model().attributeExists("cmmnCode"))
+		 .andExpect(view().name("forward:/sym/ccm/cca/EgovCcmCmmnCodeList.do"))
+		 .andExpect(forwardedUrl("/sym/ccm/cca/EgovCcmCmmnCodeList.do"));
+	}	
 
 }
